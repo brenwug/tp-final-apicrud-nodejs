698f1f48e36d2558e39da0efe7e939627063231a
Brenda <brendadianawugner@gmail.com>
2024-07-12 16:35:45 -0300

    actualización

--- models/librosModel.js
+++ models/librosModel.js
@@ -6,7 +6,8 @@ const librosModel = db.define ("libros",{
     autor:{type:DataTypes.STRING},
     titulo:{type:DataTypes.STRING},
     genero:{type:DataTypes.STRING},
-    editorial:{type:DataTypes.STRING}
+    editorial:{type:DataTypes.STRING},
+    stock_disponible:{type:DataTypes.BOOLEAN}
 })
 
 module.exports = librosModel
